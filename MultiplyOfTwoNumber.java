d3896376fd31b1075780f20e641c63d84d9999eb
Didhiti Roy <86788901+droy2002@users.noreply.github.com>
2023-02-17 23:48:18 +0530

    Add files via upload

---
+++ MultiplyOfTwoNumber.java
@@ -0,0 +1,16 @@
+//Java Program to Multiply two Floating Point Numbers
+
+import java.util.*;
+
+public class MultiplyOfTwoNumber {
+   public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    float a= sc.nextFloat();
+    float b= sc.nextFloat();
+    float multy= a*b;
+    System.out.println("Multiply of two number is:");
+    System.out.println(multy);
+   } 
+}
+
